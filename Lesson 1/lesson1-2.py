bc5ee159e0d9ae5111815b119e7986215af31068
rinatsabitov <sabitovrinat@gmail.com>
2021-07-18 12:50:27 -0400

    вторая задача первой домашки

---
+++ Lesson 1/lesson1-2.py
@@ -0,0 +1,14 @@
+five = "101"
+six = "110"
+print("Bit sequence for five is " + five)
+print("Bit sequence for six is " + six + "\n")
+
+print("Logical AND for five(101) and six(110) is four(100)")
+print("Logical OR for five(101) and six(110) is seven(111)")
+print("Logical XOR for five(101) and six(110) is three(011)\n")
+
+print("2 digits left circular shift for five(101) is six(110)")
+print("2 digits left circular shift for six(110) is three(011)\n")
+
+print("2 digits right circular shift for five(101) is three(011)")
+print("2 digits right circular shift for six(110) is five(101)")
