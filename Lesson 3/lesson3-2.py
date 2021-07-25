bf2e4338ea8f2a718826b7a50ca8fd6452eaec12
rinatsabitov <sabitovrinat@gmail.com>
2021-07-25 18:35:28 -0400

    Вторая задача третей домашки

---
+++ Lesson 3/lesson3-2.py
@@ -0,0 +1,8 @@
+initial_list = [8, 3, 15, 6, 4, 2]
+even_members_index_list = []
+
+for i in range(len(initial_list)):
+    if initial_list[i] % 2 == 0:
+        even_members_index_list.append(i)
+
+print(even_members_index_list)
