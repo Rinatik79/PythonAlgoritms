89413baf9690a5d39d380764b8ef40d3f649f619
rinatsabitov <sabitovrinat@gmail.com>
2021-07-26 01:59:25 -0400

    восьмая задача третей домашки

--- Lesson 3/lesson3-7.py
+++ Lesson 3/lesson3-7.py
@@ -1,5 +1,14 @@
-list = [-2, 3, -4400, 5, 6, 7, -8, 9, 0, 2, 3, 4, 5, 5, 6, 6, -777, 7, 7, 7]
-sorted_list = list.copy()
-sorted_list.sort()
+matrix = []
+print("Let's create matrix 5 x 5 by entering 5 lines with 4 elements separated by ' '.")
+print("The fifth element of every line will be sum of all line numbers.")
+for i in range(0, 5):
+    line = input("Please enter numbers for line number " + str(i) + ": ").split()
+    sum = 0
+    for j in range(4):
+        line[j] = int(line[j])
+        sum += line[j]
 
-print("Two lowest numbers in given list are: " + str(sorted_list[0]) + " and " + str(sorted_list[1]))
+    line.append(sum)
+    matrix.append(line)
+
+print(*matrix, sep = "\n")
