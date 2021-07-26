87ac5e9514dd342e96b1628c535924bdf83ad2bd
rinatsabitov <sabitovrinat@gmail.com>
2021-07-26 01:28:52 -0400

    седьмая задача третей домашки

---
+++ Lesson 3/lesson3-7.py
@@ -0,0 +1,5 @@
+list = [-2, 3, -4400, 5, 6, 7, -8, 9, 0, 2, 3, 4, 5, 5, 6, 6, -777, 7, 7, 7]
+sorted_list = list.copy()
+sorted_list.sort()
+
+print("Two lowest numbers in given list are: " + str(sorted_list[0]) + " and " + str(sorted_list[1]))
