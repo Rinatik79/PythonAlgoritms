5638cb8e8da9f15667856a28d1194f7c9a58e352
rinatsabitov <sabitovrinat@gmail.com>
2021-08-18 01:45:37 -0400

    Восьмая домашка первое задание. Сделал без рекурсии. У меня от рекурсии мозги кипят.

---
+++ Lesson 8/lesson8-1.py
@@ -0,0 +1,18 @@
+import hashlib
+s = "papa u vasi silen v matematike"
+#hush_obj1 = hashlib.sha1(s.encode('utf-8')).hexdigest()
+
+counter = 0
+
+for i in range(len(s)):
+
+    for j in range(i, len(s) + 1):
+        current_substring = s[i:j]
+        current_hash_obj = hashlib.md5(current_substring.encode('utf-8')).hexdigest()
+
+        if hashlib.md5(s[i:j].encode('utf-8')).hexdigest() == current_hash_obj:
+            counter += 1
+        print(s[i:j])
+
+
+print("We have " + str(counter) + " substrings totally.")
